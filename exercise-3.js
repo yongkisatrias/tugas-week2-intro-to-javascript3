be64270b72839742a39837c04e6dd9a71b0c16e6
Yongki Satria Sanjaya <yongkisatrias@gmail.com>
2023-09-24 23:31:28 +0700

    [CREATE][UPDATE] Membuat dan menyelesaikan exercise-3.js

---
+++ exercise-3.js
@@ -0,0 +1,41 @@
+// Program 1: Using Promises to Calculate Sums
+function addNumbers(a, b) {
+  return new Promise((resolve, reject) => {
+    if (typeof a !== "number" || typeof b !== "number") {
+      reject(new Error("Input harus berupa angka"));
+    } else {
+      const result = a + b;
+      resolve(result);
+    }
+  });
+}
+
+addNumbers(5, 10)
+  .then((result) => {
+    console.log(`Hasil penjumlahan: ${result}`);
+  })
+  .catch((error) => {
+    console.error(`Error: ${error.message}`);
+  });
+
+// Program 2: Counting the Number of Odd Numbers in an Array
+function countOddNumbers(numbers) {
+  return new Promise((resolve, reject) => {
+    if (!Array.isArray(numbers)) {
+      reject(new Error("Input harus berupa array"));
+    } else {
+      const oddNumbers = numbers.filter((num) => num % 2 !== 0);
+      resolve(oddNumbers.length);
+    }
+  });
+}
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+countOddNumbers(numbers)
+  .then((result) => {
+    console.log(`Jumlah bilangan ganjil dalam array: ${result}`);
+  })
+  .catch((error) => {
+    console.error(error.message);
+  });
