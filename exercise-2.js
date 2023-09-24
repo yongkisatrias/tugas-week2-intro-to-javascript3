e656684f3823b87fa3763dd6f9c89f9c7ada35d8
Yongki Satria Sanjaya <yongkisatrias@gmail.com>
2023-09-24 22:10:17 +0700

    [CREATE][UPDATE] Membuat exercise-2.js dan menyelesaikan exercise-2.js

---
+++ exercise-2.js
@@ -0,0 +1,23 @@
+const getMonth = (callback) => {
+  setTimeout(() => {
+    let error = false;
+    let month = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"];
+    if (!error) {
+      callback(null, month);
+    } else {
+      callback(new Error("Sorry data not found"), []);
+    }
+  }, 4000);
+};
+
+// Callback function to display the month
+const displayMonths = (error, months) => {
+  if (months) {
+    months.map((month) => console.log(month));
+  } else {
+    console.log(error.message);
+  }
+};
+
+// Execute the getMonth function with the displayMonths callback
+getMonth(displayMonths);
