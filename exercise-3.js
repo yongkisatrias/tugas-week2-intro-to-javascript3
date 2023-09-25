f39447ecedb606a018db90e0e1c869fad182c1bc
Yongki Satria Sanjaya <121309496+yongkisatrias@users.noreply.github.com>
2023-09-25 21:47:36 +0700

    Update exercise-3.js

--- exercise-3.js
+++ exercise-3.js
@@ -1,14 +1,12 @@
 // Program 1: Using Promises to Calculate number
-function addNumbers(a, b) {
-  return new Promise((resolve, reject) => {
-    if (typeof a !== "number" || typeof b !== "number") {
-      reject(new Error("Input harus berupa angka"));
-    } else {
-      const result = a + b;
-      resolve(result);
-    }
-  });
-}
+const addNumbers = (a, b) => new Promise((resolve, reject) => {
+  if (typeof a !== "number" || typeof b !== "number") {
+    reject(new Error("Input harus berupa angka"));
+  } else {
+    const result = a + b;
+    resolve(result);
+  }
+});
 
 addNumbers(5, 10)
   .then((result) => {
@@ -17,19 +15,18 @@ addNumbers(5, 10)
   .catch((error) => {
     console.log(error.message);
   });
+
 // Output : Hasil penjumlahan: 15
 
 // Program 2: Using Promises to counting the Number of Odd Numbers in an Array
-function countOddNumbers(numbers) {
-  return new Promise((resolve, reject) => {
-    if (!Array.isArray(numbers)) {
-      reject(new Error("Input harus berupa array"));
-    } else {
-      const oddNumbers = numbers.filter((num) => num % 2 !== 0);
-      resolve(oddNumbers.length);
-    }
-  });
-}
+const countOddNumbers = (numbers) => new Promise((resolve, reject) => {
+  if (!Array.isArray(numbers)) {
+    reject(new Error("Input harus berupa array"));
+  } else {
+    const oddNumbers = numbers.filter((num) => num % 2 !== 0);
+    resolve(oddNumbers.length);
+  }
+});
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
